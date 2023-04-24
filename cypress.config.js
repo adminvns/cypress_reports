12915c39ad7696fe6aff022b020a1a2ebce7872f
adminvns <shubham.editor@gmail.com>
2023-04-25 02:08:00 +0530

    added xml testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("cypress");
  require('cypress-mochawesome-reporter/plugin')
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+ // reporter: 'cypress-mochawesome-reporter',
   video:false,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-        screenshotOnRunFailure= true;
-        require('cypress-mochawesome-reporter/plugin')(on);
+      // // implement node event listeners here
+      //   screenshotOnRunFailure= true;
+      //   require('cypress-mochawesome-reporter/plugin')(on);
        
     },
   },
